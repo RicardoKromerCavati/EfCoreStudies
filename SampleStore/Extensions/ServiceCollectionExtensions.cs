fe684770daefc36ae85f1c70f3a57e789a3421dd
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 12:24:37 -0300

    chore: Create extension method to configure DI.

---
+++ SampleStore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleStoreApi.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void ConfigureDependencyInjection(this IServiceCollection services)
+		{
+			const string appSettingsJsonFile = "appsettings.json";
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile(appSettingsJsonFile)
+				.Build();
+
+			services.AddControllers();
+			
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+
+			services.AddDbContext<ApplicationDbContext>(options =>
+			{
+				const string connectionStringName = "ConnectionString";
+				options.UseSqlServer(configuration.GetConnectionString(connectionStringName));
+			}, contextLifetime: ServiceLifetime.Scoped);
+		}
+	}
+}
