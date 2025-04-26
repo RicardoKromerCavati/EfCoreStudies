626230c57902e8bbe43bc0c21e90730af20d435d
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 12:20:10 -0300

    chore: Add Cpf column to customer class.

--- Core/Entities/Customer.cs
+++ Core/Entities/Customer.cs
@@ -6,8 +6,9 @@
         // [Required]
         // [Column("Name")]
         public required string Name { get; set; }
+		public required string Cpf { get; set; }
         public DateTime BirthDate { get; set; }
 
-        public ICollection<Order> Orders { get; set; }
+		public ICollection<Order> Orders { get; set; }
     }
 }
