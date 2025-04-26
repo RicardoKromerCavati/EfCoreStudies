2c6b38ed828e52807919eb3cb47c88bbee2abf9c
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 16:23:06 -0300

    chore: Create EntityResult to use in controllers.

---
+++ Core/Models/EntityResult.cs
@@ -0,0 +1,29 @@
+﻿namespace Core.Models
+{
+	public class EntityResult<T> where T : class
+	{
+		public T? Entity { get; private set; } = null;
+		public string ErrorMessage { get; private set; } = string.Empty;
+		public bool IsSuccess { get; private set; }
+
+		private EntityResult(string errorMessage)
+		{
+			ErrorMessage = errorMessage;
+			IsSuccess = false;
+		}
+
+		private EntityResult(T entity)
+		{
+			Entity = entity;
+			IsSuccess = true;
+		}
+
+		private EntityResult()
+		{
+			
+		}
+
+		public static EntityResult<T> CreateSuccessfulResult(T entity) => new(entity);
+		public static EntityResult<T> CreateErrorResult(string errorMessage) => new(errorMessage);
+	}
+}
