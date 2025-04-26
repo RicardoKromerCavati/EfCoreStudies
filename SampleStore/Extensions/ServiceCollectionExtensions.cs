54a7eca126cd44cec34241b04850f71a87d6e111
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 16:24:37 -0300

    chore: Create specific methods to configure DI.

--- SampleStore/Extensions/ServiceCollectionExtensions.cs
+++ SampleStore/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Infrastructure.Repositories;
+﻿using Core.Entities;
+using Core.Repositories;
+using Infrastructure;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace SampleStoreApi.Extensions
@@ -7,21 +10,37 @@ namespace SampleStoreApi.Extensions
 	{
 		public static void ConfigureDependencyInjection(this IServiceCollection services)
 		{
+			services.AddControllers();
+
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+
+			services.ConfigureDatabase();
+			services.ConfigureRepositories();
+		}
+
+		private static void ConfigureRepositories(this IServiceCollection services)
+		{
+			services.AddScoped<ICustomerRepository, CustomerRepository>();
+			services.AddScoped<IBookRepository, BookRepository>();
+			services.AddScoped<IOrderRepository, OrderRepository>();
+		}
+
+		private static void ConfigureDatabase(this IServiceCollection services)
+		{
 			const string appSettingsJsonFile = "appsettings.json";
+
 			var configuration = new ConfigurationBuilder()
 				.AddJsonFile(appSettingsJsonFile)
 				.Build();
 
-			services.AddControllers();
-			
-			services.AddEndpointsApiExplorer();
-			services.AddSwaggerGen();
-
 			services.AddDbContext<ApplicationDbContext>(options =>
 			{
 				const string connectionStringName = "ConnectionString";
-				options.UseSqlServer(configuration.GetConnectionString(connectionStringName));
+				options
+				.UseSqlServer(configuration.GetConnectionString(connectionStringName));
 			}, contextLifetime: ServiceLifetime.Scoped);
 		}
 	}
+
 }
