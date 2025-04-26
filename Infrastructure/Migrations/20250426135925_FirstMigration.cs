f1f8709598e892ae79e20658d304e44af8c18782
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 12:21:31 -0300

    chore: Add migrations.

---
+++ Infrastructure/Migrations/20250426135925_FirstMigration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Publisher = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "DATETIME", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "DATETIME", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "INT", nullable: false),
+                    BookId = table.Column<int>(type: "INT", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "DATETIME", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderTable", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderTable_BookTable_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BookTable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderTable_CustomerTable_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CustomerTable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderTable_BookId",
+                table: "OrderTable",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderTable_CustomerId",
+                table: "OrderTable",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderTable");
+
+            migrationBuilder.DropTable(
+                name: "BookTable");
+
+            migrationBuilder.DropTable(
+                name: "CustomerTable");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250426142632_AddCpfColumnOnCustomer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCpfColumnOnCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "CustomerTable",
+                type: "VARCHAR(11)",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "CustomerTable");
+        }
+    }
+}
