69ec7261afde525df5dbe8011b068746cf554248
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 12:52:05 -0300

    chore: Change configuration folder location.

--- Infrastructure/Repositories/Configurations/BookConfiguration.cs
+++ Infrastructure/Repositories/Configurations/BookConfiguration.cs
@@ -2,7 +2,7 @@ using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infrastructure.Configurations;
+namespace Infrastructure.Repositories.Configurations;
 
 public class BookConfiguration : IEntityTypeConfiguration<Book>
 {
