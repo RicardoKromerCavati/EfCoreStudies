d10aae9c2e8fc70e8090dd2546a8a102786323c6
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-04-26 16:24:49 -0300

    feat: Create controllers.

---
+++ SampleStore/Controllers/BookController.cs
@@ -0,0 +1,126 @@
+﻿using Core.Entities;
+using Core.Inputs;
+using Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleStoreApi.Controllers
+{
+	[ApiController]
+	[Route("/[controller]")]
+	public class BookController : ControllerBase
+	{
+		private readonly IBookRepository _bookRepository;
+
+		public BookController(IBookRepository bookRepository)
+		{
+			_bookRepository = bookRepository;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			try
+			{
+				var books = _bookRepository.SelectAll();
+
+				if (books is null || books.Count <= 0)
+				{
+					return NotFound();
+				}
+
+				return Ok(books);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpGet("{id:int}")]
+		public IActionResult Get([FromRoute] int id)
+		{
+			try
+			{
+				var book = _bookRepository.SelectById(id);
+
+				if (book is null)
+				{
+					return NotFound();
+				}
+
+				return Ok(book);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		//public async ValueTask<IActionResult> Post([FromBody] BookInput bookInput)
+		[HttpPost]
+		public IActionResult Post([FromBody] BookInput bookInput)
+		{
+			try
+			{
+				var book = new Book
+				{
+					Name = bookInput.Name,
+					Publisher = bookInput.Publisher,
+				};
+
+				_bookRepository.Insert(book);
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPut]
+		public IActionResult Put([FromBody] BookUpdateInput bookUpdateInput)
+		{
+			try
+			{
+				var book = _bookRepository.SelectById(bookUpdateInput.Id);
+
+				if (book is null)
+				{
+					return NotFound();
+				}
+
+				book.Name = bookUpdateInput.Name;
+				book.Publisher = bookUpdateInput.Publisher;
+
+				_bookRepository.Update(book);
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpDelete("{id:int}")]
+		public IActionResult Delete([FromRoute] int id)
+		{
+			try
+			{
+				var result = _bookRepository.Delete(id);
+
+				if (result is false)
+				{
+					return NotFound();
+				}
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
---
+++ SampleStore/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleStoreApi.Controllers
+{
+	[ApiController]
+	[Route("/[controller]")]
+	public class CustomerController : ControllerBase
+	{
+		private readonly ICustomerRepository _customerRepository;
+
+		public CustomerController(ICustomerRepository customerRepository)
+		{
+			_customerRepository = customerRepository;
+		}
+
+		[HttpGet("orders-in-last-six-months/{id:int}")]
+		public IActionResult Get([FromRoute] int id)
+		{
+			try
+			{
+				var customerResult
+					= _customerRepository.GetOrdersInLastSixMonths(id);
+
+				if (customerResult.IsSuccess is false)
+				{
+					return NotFound(customerResult.ErrorMessage);
+				}
+
+				return Ok(customerResult.Entity);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
